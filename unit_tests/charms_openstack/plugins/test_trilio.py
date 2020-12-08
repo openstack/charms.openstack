2520764264e10d1ec6077f5472c038ab462df1c2
Liam Young <liam.young@canonical.com>
2020-12-08 09:49:56 +0000

    Support upgrades to Trilio 4.1
    Adding support for Trilio 4.1 includes the following changes:
    
    * Add Trilio_properties config property to enables templates to
      distinguish between 4.0 and 4.1 release.
    * Add get_trilio_codename_install_source to attempt to derive the
      Trilio version supported by an apt repo.
    * Add get_trilio_charm_instance which overrides the default
      get_charm_instance.  This will pick the correct charm class based
      on both the Trilio release and the OpenStack release.
    * Add select_trilio_release which overrides the default
      select_release and calculates the target OpenStack and Trilio
      release.
    * Add a specialist Trilio metaclass BaseTrilioCharmMeta. This
      registers charm classes using their OpenStack release, Trilio
      release and package type.
    * Move code shared between TrilioVaultCharm &
      TrilioVaultSubordinateCharm to TrilioVaultCharmMixin. Add support
      for Trilio upgrades to TrilioVaultCharmMixina.
    
    NOTE: An earlier version of this change
          (I5a5e5721d9a713b66f8c796896c400481e9733a2) was landed and
          reverted. It was reverted because get_trilio_charm_instance
          and select_trilio_release were both registered as handlers
          irrespective of whether a charm explicitly imported
          charms_openstack.plugins.trilio This caused reactive charms
          which used the default handlers to fail as they imported the
          Trilio functions and the default ones and only one can ever
          registered. This patch fixes this by wrapping the trilio
          function definitions inside make_* methods, so the decorator
          only registers the methods when the make_* methods are
          explicitly called.
    
    Change-Id: Id3bb13aff6d0e6df2d5ec144689c992cf09c1b4c

--- unit_tests/charms_openstack/plugins/test_trilio.py
+++ unit_tests/charms_openstack/plugins/test_trilio.py
@@ -2,8 +2,9 @@ import unittest.mock as mock
 import os
 
 from unit_tests.charms_openstack.charm.utils import BaseOpenStackCharmTest
-from unit_tests.utils import patch_open
+from unit_tests.utils import BaseTestCase, patch_open
 
+import charms_openstack.charm.core as co_core
 import charms_openstack.plugins.trilio as trilio
 
 
@@ -12,6 +13,11 @@ class TrilioVaultFoobar(trilio.TrilioVaultCharm):
     abstract_class = True
     name = 'test'
     all_packages = ['foo', 'bar']
+    os_release_pkg = 'nova-common'
+
+    @classmethod
+    def trilio_version_package(cls):
+        return "dmapi"
 
 
 class TrilioVaultFoobarSubordinate(trilio.TrilioVaultSubordinateCharm):
@@ -114,6 +120,7 @@ class TestTrilioCommonBehaviours(BaseOpenStackCharmTest):
         self.patch_object(trilio.ch_core.hookenv, "status_set")
         self.patch_object(trilio.fetch, "filter_installed_packages")
         self.patch_object(trilio.fetch, "apt_install")
+        self.patch_object(trilio.fetch.apt_pkg, 'version_compare')
         self.patch_object(trilio.reactive, "is_flag_set")
         self.patch_object(trilio.reactive, "clear_flag")
         self.patch_target('update_api_ports')
@@ -162,13 +169,216 @@ class TestTrilioCommonBehaviours(BaseOpenStackCharmTest):
             )
             _file.write.assert_called_once_with('testsource')
 
+    def test_trilio_properties(self):
+        cls_mock = mock.MagicMock()
+        cls_mock.charm_instance.release_pkg_version = lambda: '4.0'
+        self.version_compare.return_value = 0
+        self.assertEqual(
+            trilio.trilio_properties(cls_mock),
+            {'db_type': 'dedicated', 'transport_type': 'dmapi'})
+        self.version_compare.return_value = -1
+        self.assertEqual(
+            trilio.trilio_properties(cls_mock),
+            {'db_type': 'legacy', 'transport_type': 'legacy'})
+
+    def test_get_trilio_codename_install_source(self):
+        self.assertEqual(
+            trilio.get_trilio_codename_install_source(
+                'deb [trusted=yes] https://apt.fury.io/triliodata-4-0/ /'),
+            '4.0')
+        self.assertEqual(
+            trilio.get_trilio_codename_install_source(
+                'deb [trusted=yes] https://apt.fury.io/triliodata-4-0-0/ /'),
+            '4.0')
+        with self.assertRaises(AssertionError):
+            trilio.get_trilio_codename_install_source(
+                'deb [trusted=yes] https://apt.fury.io/triliodata/ /')
+
+    def test_get_trilio_charm_instance(self):
+        _safe_gcif = co_core._get_charm_instance_function
+        co_core._get_charm_instance_function = None
+
+        class BaseClass():
+            def __init__(self, release, *args, **kwargs):
+                pass
+
+        class Pike39(BaseClass):
+            release = 'pike'
+            trilio_release = '3.9'
+
+        class Queens40(BaseClass):
+            release = 'queens'
+            trilio_release = '4.0'
+
+        class Queens41(BaseClass):
+            release = 'queens'
+            trilio_release = '4.1'
+
+        class Rocky40(BaseClass):
+            release = 'rocky'
+            trilio_release = '4.0'
+
+        def _version_compare(ver1, ver2):
+            if float(ver1) > float(ver2):
+                return 1
+            elif float(ver1) < float(ver2):
+                return -1
+            else:
+                return 0
+
+        save_releases = trilio._trilio_releases
+        self.version_compare.side_effect = _version_compare
+        trilio._trilio_releases = {
+            'pike': {
+                trilio.AptPkgVersion('3.9'): {
+                    'deb': Pike39}},
+            'queens': {
+                trilio.AptPkgVersion('4.0'): {
+                    'deb': Queens40},
+                trilio.AptPkgVersion('4.1'): {
+                    'deb': Queens41}},
+            'rocky': {
+                trilio.AptPkgVersion('4.0'): {
+                    'deb': Rocky40}}}
+        trilio.make_trilio_get_charm_instance_handler()
+        # Check with no release being supplied. Should return the
+        # highest release class.
+        self.assertIsInstance(
+            co_core.get_charm_instance(),
+            Rocky40)
+        self.assertIsInstance(
+            co_core.get_charm_instance(release='queens_4.0'),
+            Queens40)
+        self.assertIsInstance(
+            co_core.get_charm_instance(release='queens_4.1'),
+            Queens41)
+        # Ensure an error is raised if a class satisfying the trilio condition
+        # is not found for the highest matching OpenStack class.
+        with self.assertRaises(RuntimeError):
+            co_core.get_charm_instance(release='rocky_3.9')
+        # Match the openstack release and then the closest trilio releases
+        # within that subset.
+        self.assertIsInstance(
+            co_core.get_charm_instance(release='rocky_4.1'),
+            Rocky40)
+        with self.assertRaises(RuntimeError):
+            co_core.get_charm_instance(release='icehouse_4.1')
+        trilio._trilio_releases = save_releases
+        co_core._get_charm_instance_function = _safe_gcif
+
+    def test_select_trilio_release(self):
+        def get_charm_class(release_pkg='trilio_pkg', package_version='4.0',
+                            os_codename_exception=None,
+                            version_package='trilio_pkg',
+                            package_version_exception=None,
+                            os_release_pkg='nova_pkg',
+                            os_codename_pkg='queens',
+                            trilio_source='deb https://a.io/trilio-4-2-0/ /'):
+
+            class _TrilioCharm():
+
+                def __init__(self):
+                    self.release_pkg = release_pkg
+                    self.version_package = version_package
+                    self.os_release_pkg = os_release_pkg
+                    self.source_config_key = 'openstack-origin'
+                    self.package_codenames = {}
+                    self.package_version = package_version
+                    self.os_codename_exception = os_codename_exception
+                    self.os_codename_pkg = os_codename_pkg
+                    self.trilio_source = trilio_source
+
+                @staticmethod
+                def get_os_codename_package(pkg, code_names,
+                                            apt_cache_sufficient=True):
+                    if os_codename_exception:
+                        raise os_codename_exception
+                    else:
+                        return os_codename_pkg
+
+                @staticmethod
+                def get_package_version(pkg, apt_cache_sufficient=True):
+                    if package_version_exception:
+                        raise package_version_exception
+                    else:
+                        return package_version
+
+            return _TrilioCharm()
+
+        _safe_rsf = co_core._release_selector_function
+        co_core._release_selector_function = None
+        self.patch_object(
+            trilio.os_utils,
+            "get_installed_semantic_versioned_packages")
+        self.patch_object(trilio.os_utils, "os_release")
+        self.patch_object(trilio.unitdata, "kv")
+        kv_mock = mock.MagicMock()
+        self.kv.return_value = kv_mock
+        kv_mock.get.return_value = None
+        self.patch_object(
+            trilio.charms_openstack.charm.core,
+            "get_charm_instance")
+
+        trilio.make_trilio_get_charm_instance_handler()
+        trilio.make_trilio_select_release_handler()
+        select_trilio_release = co_core._release_selector_function
+        self.get_charm_instance.return_value = get_charm_class()
+        self.assertEqual(
+            select_trilio_release(),
+            'queens_4.0')
+
+        # Check RuntimeError is raised if release_pkg is missing from charm
+        # class
+        self.get_charm_instance.return_value = get_charm_class(
+            release_pkg=None)
+        with self.assertRaises(RuntimeError):
+            select_trilio_release()
+
+        # Test falling back to get_installed_semantic_versioned_packages
+        self.os_release.return_value = 'pike'
+        self.get_installed_semantic_versioned_packages.reset_mock()
+        self.get_installed_semantic_versioned_packages.return_value = ['nova']
+        self.get_charm_instance.return_value = get_charm_class(
+            os_codename_pkg=None)
+        self.assertEqual(
+            select_trilio_release(),
+            'pike_4.0')
+
+        # Check RuntimeError is raised if version_package is missing from charm
+        # class
+        self.get_charm_instance.return_value = get_charm_class(
+            version_package=None)
+        with self.assertRaises(RuntimeError):
+            select_trilio_release()
+
+        # Test falling back to get_trilio_codename_install_source
+        self.get_charm_instance.return_value = get_charm_class(
+            package_version_exception=ValueError)
+        self.assertEqual(
+            select_trilio_release(),
+            'queens_4.2')
+        co_core._release_selector_function = _safe_rsf
+
 
 class TestTrilioVaultCharm(BaseOpenStackCharmTest):
 
     def setUp(self):
         super().setUp(TrilioVaultFoobar, {})
+        self.patch_object(trilio.ch_core.hookenv, "log")
+        self.patch_object(trilio.ch_core.hookenv, "status_set")
+        self.patch_object(
+            trilio.charms_openstack.charm.core,
+            "get_charm_instance")
         self.patch_object(trilio, "_install_triliovault")
         self.patch_object(trilio, "_configure_triliovault_source")
+        self.patch_object(trilio.fetch, "apt_update")
+        self.patch_object(trilio.fetch, "apt_install")
+        self.patch_object(trilio.fetch.apt_pkg, "version_compare")
+        self.patch_target('config')
+        self._conf = {
+            'triliovault-pkg-source': 'deb https://a.io/trilio-4-2-0/ /'
+        }
+        self.config.get.side_effect = lambda x, b=None: self._conf.get(x, b)
 
     def test_series_upgrade_complete(self):
         self.patch_object(trilio.charms_openstack.charm.HAOpenStackCharm,
@@ -189,6 +399,66 @@ class TestTrilioVaultCharm(BaseOpenStackCharmTest):
                           'configure_source')
         self.target.install()
         self._install_triliovault.assert_called_once_with(self.target)
+
+    def test_trilio_source(self):
+        self.assertEqual(
+            self.target.trilio_source,
+            'deb https://a.io/trilio-4-2-0/ /')
+
+    def test_do_trilio_pkg_upgrade(self):
+        self.target.do_trilio_pkg_upgrade()
+        self.apt_update.assert_called_once_with()
+        self.apt_install.assert_called_once_with(
+            packages=['foo', 'bar'],
+            options=[
+                '--option', 'Dpkg::Options::=--force-confnew',
+                '--option', 'Dpkg::Options::=--force-confdef'],
+            fatal=True)
+
+    def test_run_trilio_upgrade(self):
+        self.patch_target('get_os_codename_package')
+        self.get_os_codename_package.return_value = 'queens'
+        charm_cls = mock.MagicMock()
+        interface_mocks = [mock.MagicMock(), mock.MagicMock()]
+        self.get_charm_instance.return_value = charm_cls
+        self.target.run_trilio_upgrade(interfaces_list=interface_mocks)
+        self._configure_triliovault_source.assert_called_once_with()
+        charm_cls.do_trilio_pkg_upgrade.assert_called_once_with()
+        charm_cls.render_with_interfaces.assert_called_once_with(
+            interface_mocks)
+        charm_cls.do_trilio_upgrade_db_migration.assert_called_once_with()
+
+    def test_trilio_upgrade_available(self):
+        self.patch_target('get_package_version')
+        self.get_package_version.return_value = '4.1'
+        self.version_compare.return_value = 1
+        self.assertTrue(self.target.trilio_upgrade_available())
+        self.version_compare.assert_called_once_with('4.2', '4.1')
+
+    def test_upgrade_if_available(self):
+        self.patch_target('openstack_upgrade_available')
+        self.patch_target('trilio_upgrade_available')
+        self.patch_target('run_upgrade')
+        self.patch_target('run_trilio_upgrade')
+        interface_mocks = [mock.MagicMock(), mock.MagicMock()]
+
+        self._conf['action-managed-upgrade'] = False
+        self.openstack_upgrade_available.return_value = True
+        self.trilio_upgrade_available.return_value = True
+        self.target.upgrade_if_available(interface_mocks)
+        self.run_upgrade.assert_called_once_with(
+            interfaces_list=interface_mocks)
+        self.run_trilio_upgrade.assert_called_once_with(
+            interfaces_list=interface_mocks)
+
+        self.run_upgrade.reset_mock()
+        self.run_trilio_upgrade.reset_mock()
+        self._conf['action-managed-upgrade'] = True
+        self.openstack_upgrade_available.return_value = True
+        self.trilio_upgrade_available.return_value = True
+        self.target.upgrade_if_available(interface_mocks)
+        self.assertFalse(self.run_upgrade.called)
+        self.assertFalse(self.run_trilio_upgrade.called)
 
 
 class TestTrilioVaultSubordinateCharm(BaseOpenStackCharmTest):
@@ -197,13 +467,7 @@ class TestTrilioVaultSubordinateCharm(BaseOpenStackCharmTest):
         super().setUp(TrilioVaultFoobarSubordinate, {})
         self.patch_object(trilio, "_install_triliovault")
         self.patch_object(trilio, "_configure_triliovault_source")
-
-    def test_series_upgrade_complete(self):
-        self.patch_object(trilio.charms_openstack.charm.OpenStackCharm,
-                          'series_upgrade_complete')
-        self.patch_target('configure_source')
-        self.target.series_upgrade_complete()
-        self.configure_source.assert_called_once_with()
+        self.patch_object(trilio.fetch, "apt_update")
 
     def test_configure_source(self):
         self.patch_object(trilio.charms_openstack.charm.OpenStackCharm,
@@ -211,9 +475,100 @@ class TestTrilioVaultSubordinateCharm(BaseOpenStackCharmTest):
         self.target.configure_source()
         self._configure_triliovault_source.assert_called_once_with()
         self.configure_source.assert_not_called()
+        self.apt_update.assert_called_once_with(fatal=True)
 
-    def test_install(self):
-        self.patch_object(trilio.charms_openstack.charm.OpenStackCharm,
-                          'configure_source')
-        self.target.install()
-        self._install_triliovault.assert_called_once_with(self.target)
+
+class TestBaseTrilioCharmMeta(BaseTestCase):
+
+    def setUp(self):
+        self.save_releases = trilio._trilio_releases
+        super().setUp()
+        self.patch_object(trilio.fetch.apt_pkg, 'version_compare')
+
+        def _version_compare(ver1, ver2):
+            if float(ver1) > float(ver2):
+                return 1
+            elif float(ver1) < float(ver2):
+                return -1
+            else:
+                return 0
+        self.version_compare.side_effect = _version_compare
+
+    def tearDown(self):
+        super().tearDown()
+        trilio._trilio_releases = self.save_releases
+
+    def register_classes(self):
+
+        class TrilioQueens40(metaclass=trilio.BaseTrilioCharmMeta):
+
+            release = 'queens'
+            trilio_release = '4.0'
+
+        class TrilioQueens41(metaclass=trilio.BaseTrilioCharmMeta):
+
+            release = 'queens'
+            trilio_release = '4.1'
+
+        class TrilioRocky40(metaclass=trilio.BaseTrilioCharmMeta):
+
+            release = 'rocky'
+            trilio_release = '4.0'
+
+        return {
+            'queens_4.0': TrilioQueens40,
+            'queens_4.1': TrilioQueens41,
+            'rocky_4.0': TrilioRocky40}
+
+    def register_classes_missing_key(self):
+
+        class TrilioQueens40(metaclass=trilio.BaseTrilioCharmMeta):
+
+            release = 'queens'
+
+    def register_classes_wrong_pkg_type(self):
+
+        class TrilioQueens40(metaclass=trilio.BaseTrilioCharmMeta):
+
+            release = 'queens'
+            trilio_release = '4.1'
+            package_type = 'up2date'
+
+    def register_classes_duplicate(self):
+
+        class TrilioQueens40A(metaclass=trilio.BaseTrilioCharmMeta):
+
+            release = 'queens'
+            trilio_release = '4.0'
+
+        class TrilioQueens40B(metaclass=trilio.BaseTrilioCharmMeta):
+
+            release = 'queens'
+            trilio_release = '4.0'
+
+    def test_class_register(self):
+        charm_classes = self.register_classes()
+        self.maxDiff = None
+        self.assertEqual(
+            trilio._trilio_releases,
+            {
+                'queens': {
+                    trilio.AptPkgVersion('4.0'): {
+                        'deb': charm_classes['queens_4.0']},
+                    trilio.AptPkgVersion('4.1'): {
+                        'deb': charm_classes['queens_4.1']}},
+                'rocky': {
+                    trilio.AptPkgVersion('4.0'): {
+                        'deb': charm_classes['rocky_4.0']}}})
+
+    def test_class_register_missing_key(self):
+        with self.assertRaises(RuntimeError):
+            self.register_classes_missing_key()
+
+    def test_class_register_wrong_pkg_type(self):
+        with self.assertRaises(RuntimeError):
+            self.register_classes_wrong_pkg_type()
+
+    def test_class_register_duplicate(self):
+        with self.assertRaises(RuntimeError):
+            self.register_classes_duplicate()
